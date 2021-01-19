b91e2d09c6990731afb560c9985b74423112efb6
Johnny Carlin <jcarlin@hashicorp.com>
2021-01-19 13:17:27 -0600

    Updated root CA resources to align with identity control plane
    Creates hierchy for PKI mounts to improve ACL policy manageability:
    
    pki
    ├── il2
    ├── il4
    └── il5
        ├── p1_int
        └── p1_root

--- env/dev/config-as-code/pki/root.tf
+++ env/dev/config-as-code/pki/root.tf
@@ -1,14 +1,15 @@
 # mount point for p1 root ca
-resource "vault_mount" "p1_pki_root" {
-  path                  = "p1_pki_root"
-  type                  = "pki"
-  max_lease_ttl_seconds = 94608000 # 3 years
-  seal_wrap                = true
-  external_entropy_access  = true
+resource "vault_mount" "pki_il5_p1_root" {
+  path                    = "pki/il5/p1_root"
+  type                    = "pki"
+  max_lease_ttl_seconds   = 94608000 # 3 years
+  seal_wrap               = var.enable_seal_wrap
+  external_entropy_access = var.enable_external_entropy_access
 }
+
 # generate p1 root ca
-resource "vault_pki_secret_backend_root_cert" "p1_pki_root" {
-  backend = vault_mount.p1_pki_root.path
+resource "vault_pki_secret_backend_root_cert" "pki_il5_p1_root" {
+  backend = vault_mount.pki_il5_p1_root.path
 
   type                 = "internal"
   common_name          = "DoD P1 Root Certificate Authority"
@@ -16,24 +17,37 @@ resource "vault_pki_secret_backend_root_cert" "p1_pki_root" {
   key_type             = "rsa"
   key_bits             = 4096
   exclude_cn_from_sans = true
+
+  country      = "US"
+  province     = "Colorado"
+  locality     = "Colorado Springs"
+  organization = "Department of Defense"
 }
+
 # mount point for p1 intermediate ca
-resource "vault_mount" "p1_pki_int" {
-  path                  = "p1_pki_int"
-  type                  = "pki"
-  max_lease_ttl_seconds = 94608000 # 3 years
-  seal_wrap                = true
-  external_entropy_access  = true
+resource "vault_mount" "pki_il5_p1_int" {
+  path                    = "pki/il5/p1_int"
+  type                    = "pki"
+  max_lease_ttl_seconds   = 94608000 # 3 years
+  seal_wrap               = var.enable_seal_wrap
+  external_entropy_access = var.enable_external_entropy_access
 }
+
 # generate p1 int ca csr
-resource "vault_pki_secret_backend_intermediate_cert_request" "p1_pki_int" {
-  backend = vault_mount.p1_pki_int.path
+resource "vault_pki_secret_backend_intermediate_cert_request" "pki_il5_p1_int" {
+  backend = vault_mount.pki_il5_p1_int.path
 
   type        = "internal"
   common_name = "DoD P1 Intermediate Certificate Authority"
   key_type    = "rsa"
   key_bits    = "4096"
+
+  country      = "US"
+  province     = "Colorado"
+  locality     = "Colorado Springs"
+  organization = "Department of Defense"
 }
+
 # sign p1 int with root ca
 resource "vault_pki_secret_backend_root_sign_intermediate" "p1_pki_int" {
   backend = vault_mount.p1_pki_root.path
